9a02383ab9353a8b5550b263cf35b10f554ae8f2
albinhallander <albinhallander39@gmail.com>
2025-04-21 19:49:05 +0200

    better quality? amp

--- android-app/app/src/main/java/com/dji/sdk/sample/WebRTCMediaOptions.java
+++ android-app/app/src/main/java/com/dji/sdk/sample/WebRTCMediaOptions.java
@@ -3,7 +3,7 @@ package com.dji.sdk.sample;
 public class WebRTCMediaOptions {
     String MEDIA_STREAM_ID = "DRONE";
     String VIDEO_SOURCE_ID = "DRONEv0";
-    int VIDEO_RESOLUTION_WIDTH = 320;
-    int VIDEO_RESOLUTION_HEIGHT = 240;
+    int VIDEO_RESOLUTION_WIDTH = 640;
+    int VIDEO_RESOLUTION_HEIGHT = 380;
     int FPS = 30;
 }
